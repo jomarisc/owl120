a4ace0a5408d159211cefaff49a8aa63bb3d4425
Charles Advincula <cadvincu@ucsc.edu>
2019-05-25 18:55:41 -0700

    Added physics to friend prefab

--- phaser/owlGame/js/prefabs/friend.js
+++ phaser/owlGame/js/prefabs/friend.js
@@ -1,6 +1,6 @@
 // Friend Prefab
 
-function Friend(game, x, y, key, frame, scale)
+function Friend(game, x, y, key, frame, scale, platformGroup)
 {
 	Phaser.Sprite.call(this, game, x, y, key, frame);
 
@@ -9,6 +9,23 @@ function Friend(game, x, y, key, frame, scale)
 	// Sprite
 	this.anchor.setTo(0.5);
 	this.scale.setTo(scale);
+
+	// Physics
+	this.AIR_SPEED = 1000;
+	this.GROUND_SPEED = 300;
+	this.JUMP_SPEED = 600;
+	this.ACCELERATION = 3000; // Max Acceleration
+	this.DRAG = 2000;
+	game.physics.enable(this); // Enable Physics Body
+	this.body.collideWorldBounds = true; // Confine Sprite to Game Camera
+	this.body.gravity.y = 1000; // Gravity
+	this.body.maxVelocity.setTo(this.GROUND_SPEED, this.JUMP_SPEED);
+	this.body.acceleration.setTo(0);
+	this.body.drag.setTo(this.DRAG, 0);
+
+	// Character
+	this.platforms = platformGroup;
+	this.jumps = 1;
 }
 
 Friend.prototype = Object.create(Phaser.Sprite.prototype);
@@ -16,5 +33,5 @@ Friend.prototype.constructor = Friend;
 
 Friend.prototype.update = function()
 {
-
+	
 }
